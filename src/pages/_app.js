fefca57a3643120eeb7afd0ec1db78264ae95d64
xiliourt <dyl.sec.ure@gmail.com>
2025-06-06 12:22:44 +1000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import '../styles/globals.css'; // Import the global styles
+import '../../styles/globals.css'; // Import the global styles
 
 function MyApp({ Component, pageProps }) {
   return (
