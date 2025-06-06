08b05e68c697027a627f1a47ab32ebf5b2e70a86
xiliourt <dyl.sec.ure@gmail.com>
2025-06-06 12:19:50 +1000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,20 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Head from 'next/head';
+import '../styles/globals.css'; // Import the global styles
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Link to Google Fonts for 'Inter'. Place this here for global font availability. */}
+        <link
+          href="[https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap](https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap)"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
