06d5a20739a42f0e5bacf319860f1ef6dc2ffcab
xiliourt <dyl.sec.ure@gmail.com>
2025-06-06 12:41:42 +1000

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Uncomment the following line to build a static site.
-  // output: "export",
-
-  reactStrictMode: true,
-};
-
-export default nextConfig;
